280602a26701100293792b68d1df3c72b0f396ea
madhav040788 <madhavghatolsoft@gmail.com>
2025-01-08 23:12:58 +0530

    both are star paturn

--- Java-Start-paturns/src/main/java/org/example/RightAngledTriangle.java
+++ Java-Start-paturns/src/main/java/org/example/RightAngledTriangle.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class RightAngledTriangle {
     public static void main(String[] args) {
-        int rows = 5;
+        int rows = 5 ;
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(" * ");
             }
             System.out.println();
         }
