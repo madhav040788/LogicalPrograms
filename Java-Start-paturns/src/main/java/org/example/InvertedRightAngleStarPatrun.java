06dff4f94e8a99dfa7275e2eba62cee94f823cb5
madhav040788 <madhavghatolsoft@gmail.com>
2025-01-11 22:43:57 +0530

    inverted RightAngle star

--- Java-Start-paturns/src/main/java/org/example/InvertedRightAngleStarPatrun.java
+++ Java-Start-paturns/src/main/java/org/example/InvertedRightAngleStarPatrun.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public class InvertedRightAngleStarPatrun {
-}
+
+        public static void main(String[] args) {
+            int n = 5; // Height of the triangle
+            for (int i = n; i >= 1; i--) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
