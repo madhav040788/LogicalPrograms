63ed8c9c035ab85c3afbb925c84e5fdb3b4a7570
madhav040788 <madhavghatolsoft@gmail.com>
2025-01-11 22:49:50 +0530

    dimond patturn with imp coment

--- Java-Start-paturns/src/main/java/org/example/DiamondPattern.java
+++ Java-Start-paturns/src/main/java/org/example/DiamondPattern.java
@@ -1,26 +1,35 @@
 package org.example;
 
 public class DiamondPattern {
-    public static void main(String[] args) {
-        int rows = 5;
-        for (int i = 1; i <= rows; i++) {//for rows
-            for (int j = rows; j > i; j--) {//for columns
-                System.out.print(" ");
+
+        public static void main(String[] args) {
+            int n = 5; // Height of half the diamond
+            // Upper part of the diamond
+            for (int i = 1; i <= n; i++) {
+                // Print spaces
+                for (int j = i; j < n; j++) {
+                    System.out.print(" ");
+                }
+                // Print stars
+                for (int k = 1; k <= (2 * i - 1); k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
+
+            // Lower part of the diamond
+            for (int i = n - 1; i >= 1; i--) {
+                // Print spaces
+                for (int j = n; j > i; j--) {
+                    System.out.print(" ");
+                }
+                // Print stars
+                for (int k = 1; k <= (2 * i - 1); k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
-        for (int i = rows - 1; i >= 1; i--) {
-            for (int j = rows; j > i; j--) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            System.out.println();
         }
     }
-}
+
 
