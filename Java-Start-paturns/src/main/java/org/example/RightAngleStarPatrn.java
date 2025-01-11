1ed030677fe5119d48acf9ac19e3f938ab8de897
madhav040788 <madhavghatolsoft@gmail.com>
2025-01-11 22:46:30 +0530

    Right Angle start paturn

--- Java-Start-paturns/src/main/java/org/example/RightAngleStarPatrn.java
+++ Java-Start-paturns/src/main/java/org/example/RightAngleStarPatrn.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public class RightAngleStarPatrn {
+
+        public static void main(String[] args) {
+            int n = 5; // Height of the triangle
+            for (int i = 1; i <= n; i++) { // i = rows
+                for (int j = 1; j <= i; j++) { // j=columns
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
 }
